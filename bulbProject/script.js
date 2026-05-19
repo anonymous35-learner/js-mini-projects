4a008b972bf51d292853200488f5dbf10f37e492
anonymous35-learner <ayushi121104@gmail.com>
2026-05-20 01:28:17 +0530

    l

--- bulbProject/script.js
+++ bulbProject/script.js
@@ -1,17 +1,37 @@
-var bulb = document.querySelector("#bulb")
-var btn =document.querySelector("button")
+// var bulb = document.querySelector("#bulb")
+// var btn =document.querySelector("button")
 
-var flag =0
+// var flag =0
 
-btn.addEventListener("click",function(){
-    if(flag == 0){
-    bulb.style.backgroundColor = "yellow"
-    console.log("clicked");
-    flag = 1;
+// btn.addEventListener("click",function(){
+//     if(flag == 0){
+//     bulb.style.backgroundColor = "yellow"
+//     console.log("clicked");
+//     flag = 1;
     
-    }else{
-         bulb.style.backgroundColor = "transparent"
-    console.log("Again clicked");
-    flag = 0;
-    }
-})
+//     }else{
+//          bulb.style.backgroundColor = "transparent"
+//     console.log("Again clicked");
+//     flag = 0;
+//     }
+// })
+
+// var h = document.querySelectorAll("h1")
+// console.log(h)
+
+// h.forEach(function(e){
+//     console.log(e);
+    
+// })
+
+
+// document.getElementById("box")
+// document.getElementsByClassName("box")
+// var h = document.querySelector("h1")
+// h.innerHTML = "bye"
+// console.log(h);
+
+var box = document.querySelector("#box")
+box.innerHTML = "<h1> Hiiiiii </h1>"
+
+box.textContent = "<h1> Hiiiiii </h1>"
